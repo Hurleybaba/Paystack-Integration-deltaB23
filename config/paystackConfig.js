317b298cc94510a2c6b14e6077ad3da25e208081
TimothyEniola <timothyyemitan@gmail.com>
2025-11-03 20:55:31 +0100

    Update Paystack configuration to use environment variable for base URL

--- config/paystackConfig.js
+++ config/paystackConfig.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
-
+const PAYSTACK_BASE_URL = process.env.PAYSTACK_BASE_URL ;
 const paystack = axios.create({
-  baseURL: "https://api.paystack.co",
+  baseURL: PAYSTACK_BASE_URL,
   headers: {
     Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
     "Content-Type": "application/json",
