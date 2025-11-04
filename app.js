0a9d1c02bdf80eb1a2344ab012d05bfc268cac90
ERIKEFE-DAVID <dafenone2006@gmail.com>
2025-11-04 09:37:49 +0100

    added webhooks

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import webhookRoutes from "./routes/webhookRoutes.js"
+
 
 dotenv.config();
-
-
 const app = express();
 app.use(express.json());
+app.use("/api", webhookRoutes);
 const PORT = process.env.PORT || 3000;
 
 
